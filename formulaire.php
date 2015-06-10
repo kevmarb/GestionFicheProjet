e98fd1f7f5ae8daf2f56d294384bf8ea435c2048
Vincent <b.vincent.iim@gmail.com>
2015-06-10 15:09:53 +0200

    version3.0.8

---
+++ formulaire.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+
+    <!-- Gumby and StyleSheet -->
+    <link rel="stylesheet" type="text/css" href="css/gumby.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+    <h3>Déposer une demande de projet</h3>
+
+    <!-- Form to submit a project -->
+    <form action="index.php">
+        <div class="row">
+
+            <div class="six columns">
+                <label for="name">Nom du Projet:</label>
+                <input type="text" name="projectName" id="name"/>
+            </div>
+
+            <div class="six columns">
+                <label for="type">Type de Projet:</label>
+                <input type="text" name="projectType" id="type"/>
+            </div>
+
+        </div>
+        <!-- ./ Row -->
+
+        <div class="row">
+
+            <div class="twelve columns">
+                <label for="description">Description du projet:</label>
+                <input type="text" name="description" id="description"/>
+            </div>
+
+        </div>
+        <!-- ./ Row -->
+
+        <div class="row">
+
+            <div class="six columns">
+                <label for="context">Contexte de la demande:</label>
+                <input type="text" name="projectName" id="name"/>
+            </div>
+
+            <div class="six columns">
+                <label for="type">Type de Projet:</label>
+                <input type="text" name="projectType" id="type"/>
+            </div>
+
+        </div>
+        <!-- ./ Row -->
+
+
+    </form>
+
+
+
+    <!-- Scripts needed for Gumby -->
+
+    <script src="js/main.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/libs/gumby.init.js"></script>
+    <script src="js/libs/gumby.min.js"></script>
+    <script src="js/libs/jquery-2.0.2.min.js"></script>
+    <script src="js/libs/jquery-2.0.2.min.map"></script>
+    <script src="js/libs/modernizr-2.6.2.min.js"></script>
+    <script src="js/libs/ui/gumby.checkbox.js"></script>
+    <script src="js/libs/ui/gumby.fixed.js"></script>
+    <script src="js/libs/ui/gumby.navbar.js"></script>
+    <script src="js/libs/ui/gumby.radiobtn.js"></script>
+    <script src="js/libs/ui/gumby.retina.js"></script>
+    <script src="js/libs/ui/gumby.skiplink.js"></script>
+    <script src="js/libs/ui/gumby.tabs.js"></script>
+    <script src="js/libs/ui/gumby.toggleswitch.js"></script>
+    <script src="js/libs/ui/jquery.validation.js"></script>
+
+    <!-- ./ Gumby Scripts -->
+
+</body>
+</html>
